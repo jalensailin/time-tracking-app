7774b3f001aff1e5345f9347ca67a9c2dd80f8b4
jalensailin <jalen.michalslevy@gmail.com>
2025-05-14 01:44:25 -0400

    simplify appReducer by removing case statement; instead, programatically get subReducer's method name

--- src/state/reducers/appReducer.ts
+++ src/state/reducers/appReducer.ts
@@ -1,41 +1,26 @@
 import { AppState } from "../../types";
-import { ADD_JOB, EDIT_JOB, DELETE_JOB } from "../actions/jobActions";
-import { ADD_CLOCK_ENTRY, EDIT_CLOCK_ENTRY, DELETE_CLOCK_ENTRY, CLOCK_IN_OUT } from "../actions/clockEntryActions";
 import { AppAction } from "../actions/appAction";
 
-import {
-  handleAddClockEntry,
-  handleClockInOut,
-  handleDeleteClockEntry,
-  handleEditClockEntry,
-} from "./clockEntryReducer";
-import { handleAddJob, handleDeleteJob, handleEditJob } from "./jobReducer";
+import * as clockEntryReducers from "./clockEntryReducer";
+import * as jobReducers from "./jobReducer";
+
+type AppReducer = (state: AppState, action: AppAction) => AppState;
+
+function toPascalCase(str: string) {
+  return str
+    .toLowerCase()
+    .replace(/(?:^|_)([a-z])/g, (_, char) => char.toUpperCase());
+}
+
+const reducers = { ...clockEntryReducers, ...jobReducers };
 
 // Root reducer that delegates to specialized reducers
-export function appReducer(state: AppState, action: AppAction): AppState {
-  switch (action.type) {
-    // Job actions
-    case ADD_JOB:
-      return handleAddJob(state, action);
-    case EDIT_JOB:
-      return handleEditJob(state, action);
-    case DELETE_JOB:
-      return handleDeleteJob(state, action);
+export const appReducer: AppReducer = (state, action) => {
+  const handlerName =
+    `handle${toPascalCase(action.type)}` as keyof typeof reducers;
 
-    // Clock entry actions
-    case ADD_CLOCK_ENTRY:
-      return handleAddClockEntry(state, action);
-    case EDIT_CLOCK_ENTRY:
-      return handleEditClockEntry(state, action);
-    case DELETE_CLOCK_ENTRY:
-      return handleDeleteClockEntry(state, action);
-    case CLOCK_IN_OUT:
-      return handleClockInOut(state, action);
+  const handler = reducers[handlerName] as AppReducer | undefined;
+  if (handler) return handler(state, action);
 
-    default:
-      // Use TypeScript to catch unhandled actions at compile time
-      //   const _exhaustiveCheck: never = action as never;
-      throw new Error();
-      return state;
-  }
-}
+  throw new Error("This action does not have an associated handler/reducer.");
+};
